5bedff4676bb3836c6907b0fb5ea819499ec7387
Reiko <captainfreedom37@gmail.com>
2020-11-07 05:37:35 +0700

    Update Kafka-Connector Version

--- cp-kafka-connect/build.sh
+++ cp-kafka-connect/build.sh
@@ -1,11 +1,11 @@
 confluent-hub install --no-prompt mongodb/kafka-connect-mongodb:latest
-confluent-hub install --no-prompt confluentinc/kafka-connect-hdfs:5.5.1
+confluent-hub install --no-prompt confluentinc/kafka-connect-hdfs:5.5.2
 
-mkdir /root/connector 
+mkdir ~/connector 
 mkdir /usr/share/java/kafka-connect-mqtt
 
-wget https://github.com/lensesio/stream-reactor/releases/download/2.0.0/kafka-connect-mqtt-2.0.0-2.4.0-all.tar.gz -O /root/connector.tar.gz \
-	&& tar -xzf /root/connector.tar.gz -C /root/connector \
-	&& rm -rf /root/connectot.tar.gz
+wget https://github.com/lensesio/stream-reactor/releases/download/2.0.0/kafka-connect-mqtt-2.0.0-2.4.0-all.tar.gz -O ~/connector.tar.gz \
+	&& tar -xzf ~/connector.tar.gz -C ~/connector \
+	&& rm -rf ~/connectot.tar.gz
 
-mv /root/connector/* /usr/share/java/kafka-connect-mqtt/
+mv ~/connector/* /usr/share/java/kafka-connect-mqtt/
