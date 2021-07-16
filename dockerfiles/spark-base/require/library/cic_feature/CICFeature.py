54cdd560c7686c9eb4fe802b5614da7986d38928
Reiko <captainfreedom37@gmail.com>
2021-07-16 16:31:48 +0700

    Modify Platform Tools and Moduls
    try to add snort but still didn't work
    change mongo-client with mongodb-chart
    update all kafka modul

---
+++ dockerfiles/spark-base/require/library/cic_feature/CICFeature.py
@@ -0,0 +1,11 @@
+# class cicfeature:
+#     def __init__(
+#         self,
+#         ip_src,
+#         ip_dst
+#         src_port
+#         dst_port
+#         protocol
+#         timestamp
+#         payloadBytes):
+#         self
